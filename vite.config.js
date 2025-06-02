df1def9a31ca47d6b12e61ee21426bc4c805d787
swstlz <malremah@asu.edu>
2025-06-02 15:01:07 -0700

    Fixed pages potentially?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/personal_website/', // ← this MUST match your repo name
   plugins: [react()],
-})
+});
